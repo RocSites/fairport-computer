0984a09cf967817a7e40ce91bfb6dacb4b0108aa
Doug Kellermeyer <doug.kellermeyer@gmail.com>
2024-01-19 11:36:17 -0500

    removed left margin

--- src/components/product-type-section.jsx
+++ src/components/product-type-section.jsx
@@ -15,7 +15,7 @@ export function ProductTypeSection(props) {
           padding: "16px",
           margin: "24px 0"
         }}>
-          <h1 style={{ textAlign: "center", fontWeight: "600", color: "black", marginLeft: "48px", marginBottom: "16px", fontSize: "2.25rem" }}>{props.filterBy}</h1>
+          <h1 style={{ textAlign: "center", fontWeight: "600", color: "black", marginBottom: "16px", fontSize: "2.25rem" }}>{props.filterBy}</h1>
           <ProductListing products={filteredProducts} />
           <Link style={{display: "flex", justifyContent: "center"}} to={`products/${slugify(props.filterBy)}`}>
             <button style={{ backgroundColor: "#016cc7", borderRadius: "16px", padding: "15px", color: "white", margin: "48px" }}>Show all {props.filterBy}</button>
