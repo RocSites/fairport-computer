fc6d3ef75e214d8356220a38ee878ec07c4d4612
Doug Kellermeyer <doug.kellermeyer@gmail.com>
2024-03-26 16:46:49 -0400

    idk why it works but it does

--- src/components/seo.jsx
+++ src/components/seo.jsx
@@ -63,13 +63,13 @@ export function Seo({
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href="/favicon-32x32.jpg"
+        href="/favicon-32x32.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href="/favicon-16x16.jpg"
+        href="/favicon-16x16.png"
       />
       <link
         rel="apple-touch-icon"
